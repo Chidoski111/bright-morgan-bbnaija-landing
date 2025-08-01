f805c256401e420bd82a3ef79d40da6761b14d03
Chidoski111 <reginahuffstuler@gmail.com>
2025-08-01 04:46:30 -0700

    push

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -52,8 +52,7 @@ export default function Home() {
       />
       <div className={styles.sliderContainer}>
         <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>Vote Bright Morgan -</p>
-          <p ref={secondText}>Vote Bright Morgan -</p>
+          <p ref={firstText}>Vote for Bright Morgan</p>
         </div>
       </div>
       <div data-scroll data-scroll-speed={0.1} className={styles.description}>
